66d779cc11fb205f0dc97ff61a2eecf89656e581
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-04-10 08:41:39 +0900

    Create ArgumentParser for handling arguments

---
+++ Sources/MarathonCore/ArgumentParser.swift
@@ -0,0 +1,85 @@
+//
+//  ArgumentParser.swift
+//  Marathon
+//
+//  Created by pixyzehn on 2017/04/09.
+//
+//
+
+import Foundation
+
+public struct ArgumentParser: CustomStringConvertible {
+    private var arguments: [ArgumentType]
+    private var index: Int
+
+    private enum ArgumentType: CustomStringConvertible {
+        case argument(String)
+        case option(String)
+        case flag(Character)
+
+        var description: String {
+            switch self {
+            case .argument(let value):
+                return value
+            case .option(let option):
+                return "--\(option)"
+            case .flag(let flag):
+                return "-\(String(flag))"
+            }
+        }
+    }
+
+    // MARK: - Init
+
+    public init(arguments: [String]) {
+        self.arguments = arguments.map { argument in
+            if argument.characters.first == "-" {
+                let flags = argument[argument.characters.index(after: argument.startIndex)..<argument.endIndex]
+
+                if flags.characters.first == "-" {
+                    let option = flags[flags.characters.index(after: flags.startIndex)..<flags.endIndex]
+                    return .option(option)
+                }
+
+                if let flag = flags.characters.first, flags.characters.count == 1 {
+                    return .flag(flag)
+                }
+            }
+
+            return .argument(argument)
+        }
+        self.index = 0
+    }
+
+    // MARK: - API
+
+    public var description:String {
+        return arguments.map { $0.description }.joined(separator: " ")
+    }
+
+    public var isEmpty: Bool {
+        return arguments.isEmpty
+    }
+
+    public func hasOption(_ optionName: String, flag flagName: Character) -> Bool {
+        var index = 0
+
+        for argument in arguments {
+            switch argument {
+            case .option(let option):
+                if option == optionName {
+                    return true
+                }
+            case .flag(let flag):
+                if flag == flagName {
+                    return true
+                }
+            default:
+                break
+            }
+            index += 1
+        }
+
+        return false
+    }
+}
