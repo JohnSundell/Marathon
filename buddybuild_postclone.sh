01447199de89732b800a51e94cbb8b0c54129ef8
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-05-26 23:24:53 +0900

    Remove `Run SwiftLint` from buddybuild_postclone

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
 swift package generate-xcodeproj
-
-# Install swiftlint if necessary
-if ! which swiftlint >/dev/null; then
-    brew install swiftlint
-fi
-
-# Run Swiftlint
-echo "Here comes the output of Swiftlint"
-swiftlint
